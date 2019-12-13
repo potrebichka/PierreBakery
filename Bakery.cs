4cea8c0c2a57b75b48cd00a48e0f3e76a1d477fd
Nina Potrebich <potrebich@gmail.com>
2019-12-13 11:06:28 -0800

    add option choice

--- Bakery.cs
+++ Bakery.cs
@@ -12,19 +12,42 @@ namespace Bakery {
 
             int myDateHour = DateTime.Now.Hour;
             if (myDateHour >= 5 && myDateHour < 12) {
-                Table.PrintAlign($"Good morning, {userName}!");
+                Table.PrintAlign(userName == "" ? $"Good morning!" : $"Good morning, {userName}!");
             } else if (myDateHour >= 12 && myDateHour < 17) {
-                Table.PrintAlign($"Good afternoon, {userName}!");
+                Table.PrintAlign(userName == "" ? $"Good morning!" : $"Good afternoon, {userName}!");
             } else if (myDateHour >= 1 && myDateHour < 5) {
-                Table.PrintAlign($"Hello {userName}!");
+                Table.PrintAlign(userName == "" ? $"Good morning!" : $"Hello {userName}!");
             } else {
-                Table.PrintAlign($"Good evening, {userName}");
+                Table.PrintAlign(userName == "" ? $"Good morning!" : $"Good evening, {userName}");
             }
 
-            //breads
             Menu menu = new Menu();
             menu.PrintMenu();
 
+            bool condition = true;
+            while (condition) {
+                Console.WriteLine("Choose option:");
+                Console.WriteLine("[1]: MENU");
+                Console.WriteLine("[2]: CART");
+                Console.WriteLine("[3]: Add item to CART");
+                Console.WriteLine("[4]: Remove item from CART");
+                Console.WriteLine("[5]: Exit program");
+
+                string userInput = Console.ReadLine();
+                switch (userInput) {
+                    case "1":
+                        menu.PrintMenu();
+                        break;
+                    case "5":
+                        condition = false;
+                        break;
+                    default:
+                        Console.WriteLine("Incorrect input");
+                        break;
+                }
+            }
+
+            Table.PrintAlign(userName == "" ? $"Good Bye!" : $"Good bye, {userName}!");
         }
     }
 }
